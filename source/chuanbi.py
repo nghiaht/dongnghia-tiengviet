be52d21b443e2219eccf26b5db5913e27e664af2
Huỳnh Trọng Nghĩa <Nghia.HT@hotmail.com>
2014-03-13 09:05:06 +0700

    Da co toan bo link, 36 nghin tu

--- source/chuanbi.py
+++ source/chuanbi.py
@@ -29,18 +29,34 @@ def GetSitemap():
 
 
 
+file_path = 'data/960.txt'
 
 # Ghi link ra file text: 'link_vn.txt'
 def GhiLinkRaFile(ten_file, set_link):
-    with open(ten_file, 'wb+') as f:
+    with open(ten_file, 'a') as f:
         for link in set_link:
             f.write(link + '\n')
 
 def LayLinkCon(link_goc):
+    
+    
     soup = BeautifulSoup( urllib.urlopen(link_goc).read() )
     set_link = set()
     for link in soup.find_all('a'):
         l = str(link.get('href'))
         if l.find('Allpages') == -1 and l.find('/dict/vn_vn') == 0:
             set_link.add(l)
-    return set_link
+
+    GhiLinkRaFile( file_path, set_link)
+
+def LayLinkToanBo(link_file):
+    with open(link_file) as f:
+        lines = f.read().split('\n')
+        for line in lines:
+            print "--> Trang: ", line
+            LayLinkCon(u'http://tratu.soha.vn' + line)
+
+
+# Lay tu dong nghia tu mot link
+def LayTuDongNghia(link):
+    soup = 
